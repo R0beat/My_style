82c21ab88707390d6155d37cecbe844ae4b81c34
Roberto Gonzalez Arriaga <roberto.gonzalez.a739@gmail.com>
2025-01-04 11:00:29 -0600

    Formularios

--- js/main.js
+++ js/main.js
@@ -1,6 +1,26 @@
 const menuResponsive = document.getElementById("menu-responsive");
 const navUl = document.querySelector("nav ul");
 
-menuResponsive.addEventListener("click", () => {
-  navUl.style.display = navUl.style.display === "block" ? "none" : "block";
-});
+if (menuResponsive) {
+  menuResponsive.addEventListener("click", (e) => {
+    navUl.style.display = navUl.style.display === "block" ? "none" : "block";
+  });
+}
+
+let modal = document.getElementById("myModal");
+let btn = document.getElementById("myBtn");
+let span = document.getElementById("close");
+let cerrar = () => {
+  modal.style.display = "none";
+};
+
+btn.onclick = function () {
+  modal.style.display = "block";
+  modal.style.transition = ".35s ease-in-out";
+};
+
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
